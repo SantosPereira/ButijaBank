a05019c00e983863066e144810bfd5239a3bd58e
SantosPereira <pedrohenriquelemam@gmail.com>
2021-03-20 20:03:38 -0300

    Início do desenvolvimento da versão Desktop com ElectronJS

--- Project/Desktop/index.js
+++ Project/Desktop/index.js
@@ -1,1 +1,10 @@
-console.log('teste!')
+const {app,BrowserWindow} = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+
+    });
+    mainWindow.loadURL(`file://${__dirname}/index.html`)
+});
