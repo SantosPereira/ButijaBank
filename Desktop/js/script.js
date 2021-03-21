b70ac30142ae618af62120b7eac21520db9eee81
SantosPereira <pedrohenriquelemam@gmail.com>
2021-03-21 12:12:02 -0300

    Add interatividade com jQuery
    Aqui comecei a adicionar interatividade a página usando jQuery. Criei uma animação para quando o formulário de acesso é enviado.

---
+++ Desktop/js/script.js
@@ -0,0 +1,6 @@
+function get(){
+    var loading = $(".loading").attr("hidden", false)
+    var acesso = $("#acesso").remove()
+    acesso = $("#acesso").append(loading)
+    // chamar animação css
+}
